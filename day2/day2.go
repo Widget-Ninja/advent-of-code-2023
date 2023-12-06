7b7884ff739ff2f5e44d2839aef86706a64411ea
Widget-Ninja <shamus.teh.jackal@gmail.com>
2023-12-06 14:30:54 -0600

    Geekfest Start

--- day2/day2.go
+++ day2/day2.go
@@ -6,6 +6,7 @@ import (
 	"bufio"
 	"strings"
 	"regexp"
+	"strconv"
 )
 
 func check(e error) {
@@ -17,37 +18,106 @@ func check(e error) {
 func main() {
 	input, err := os.Open("C:/Users/shamu/Documents/Code/advent-of-code-2023/day2/inputTest.txt")
 	check(err)
+	
 	findNumber := regexp.MustCompile("[0-9]+")
-
 	fileScan := bufio.NewScanner(input)
     fileScan.Split(bufio.ScanLines)
 	var inputLines []string
     for fileScan.Scan() {
         inputLines = append(inputLines, fileScan.Text())
     }
+	var totalGameNumbers int
+	// var enoughDice bool
 	for _, line := range inputLines {
 		var lineSplit []string
+		var gameNumber int
+		var gameCount int
+		enoughDiceArray := []int{0,0,0,0,0,0,0}
 		lineSplit = strings.Split(line, ": ")
 		// fmt.Printf("%s\n %s\n", lineSplit[0], lineSplit[1])
-		
+		for _, findGame := range lineSplit {
+			findGameRE := strings.Contains(findGame, "Game")
+			check(err)
+			// fmt.Printf("findGameRE: %t\n", findGameRE)
+			if findGameRE == true {
+				var gameNumberString string
+				gameNumberString = findNumber.FindString(findGame)
+				gameNumber, err = strconv.Atoi(string(gameNumberString))
+				check(err)
+			}
+		}
+		fmt.Printf("---------------Game Number: %d-------------\n", gameNumber)
 		for _, games := range lineSplit {
 			var game []string
 			game = strings.Split(games, "; ")
-			// for _, numberGames := range game {
-			// fmt.Printf("%s\n", numberGames)
-			// }
+			// fmt.Printf("Game Number: %d\n", gameNumber)
+			gameCount := 0
 			for _, colors := range game {
 				var color []string
+				var greenBool, redBool, blueBool bool
+				var greenStr, redStr, blueStr string
+				var greenDice, redDice, blueDice int
+				var greenTotal, redTotal, blueTotal int
 				color = strings.Split(colors, ", ")
 				for _, numberColors := range color {
-					fmt.Printf("%s\n", numberColors)
+					// fmt.Printf("%s\n", numberColors)
+					greenBool = strings.Contains(numberColors, "green")
+					redBool = strings.Contains(numberColors, "red")
+					blueBool = strings.Contains(numberColors, "blue")
+					if greenBool == true {
+						greenStr = findNumber.FindString(numberColors)
+						greenDice, err = strconv.Atoi(string(greenStr))
+						greenTotal = greenTotal + greenDice
+						// fmt.Printf("Green Total: %d\n", greenTotal)
+					}
+					if redBool == true {
+						redStr = findNumber.FindString(numberColors)
+						redDice, err = strconv.Atoi(string(redStr))
+						redTotal = redTotal + redDice
+						// fmt.Printf("Red Total: %d\n", redTotal)
+					}
+					if blueBool == true {
+						blueStr = findNumber.FindString(numberColors)
+						blueDice, err = strconv.Atoi(string(blueStr))
+						blueTotal = blueTotal + blueDice
+						// fmt.Printf("Blue Total: %d\n", blueTotal)
+					}
 				}
-				findNumber := regexp.MustCompile("[0-9]+")
-				// findGreen := regexp.MustCompile(`green`)
-				// findRed := regexp.MustCompile(`red`)
-				// findBlue := regexp.MustCompile(`blue`)
+				if greenTotal > 13 {
+					// enoughDice = false
+					enoughDiceArray[gameCount] = 0
+					fmt.Printf("Green Failed\n")
+				} else if blueTotal > 14 {
+					// enoughDice = false
+					fmt.Printf("Blue Failed\n")
+					enoughDiceArray[gameCount] = 0
+				} else if redTotal > 12 {
+					// enoughDice = false
+					fmt.Printf("Red Failed\n")
+					enoughDiceArray[gameCount] = 0
+				} else {
+					// enoughDice = true
+					enoughDiceArray[gameCount] = 1
+				}
+				fmt.Printf("enoughDiceArray:\n")
+				fmt.Print(enoughDiceArray)
+				gameCount = gameCount + 1
 			}
 		}
+		var gameWorkNumber int
+		gameWork := true
+		for _, dice := range enoughDiceArray {
+			gameWorkNumber = gameWorkNumber + dice
+			gameWorkNumber = gameWorkNumber / gameCount
+			fmt.Printf("gameWorkNumber: %d\n", gameWorkNumber)
+			if gameWorkNumber < 1 {
+				gameWork = false
+			}
+		}
+		if gameWork == true {
+			fmt.Printf("Game %d is possible!\n", gameNumber)
+			totalGameNumbers = totalGameNumbers + gameNumber
+		}
 	}
-	// fmt.Print(inputLines)
+	fmt.Print(totalGameNumbers)
 }
