9fe6bbae46fd880cc7f71dab2ff924cfc98a8caf
Widget-Ninja <shamus.teh.jackal@gmail.com>
2023-12-05 22:18:24 -0600

    Mostly day4part2...

--- day4/day4part2.go
+++ day4/day4part2.go
@@ -15,7 +15,7 @@ func check(e error) {
 }
 
 func main() {
-	input, err := os.Open("C:/Users/shamu/Documents/Code/advent-of-code-2023/day4/input.txt")
+	input, err := os.Open("C:/Users/shamu/Documents/Code/advent-of-code-2023/day4/inputTest.txt")
 	check(err)
 
 	fileScan := bufio.NewScanner(input)
@@ -24,26 +24,19 @@ func main() {
     for fileScan.Scan() {
         inputLines = append(inputLines, fileScan.Text())
     }
-	// fmt.Print(inputLines)
-	totalScore := 0
+	var totalScore int
+	var totalCount []int
 	cardCount := 1
 	for _, line := range inputLines {
 		var lineSplit []string
 		lineSplit = strings.Split(line, ": ")
-		// fmt.Printf("%s\n", lineSplit)
 		lineSplit = strings.Split(lineSplit[1], " | ")
-		// fmt.Printf("%s\n", lineSplit)
 		winner := lineSplit[0]
-		// fmt.Printf("%s\n", winner)
 		winnerNumbers := strings.Split(winner, " ")
-		// fmt.Printf("%s\n", winnerNumbers[0])
 		card := lineSplit[1]
-		// fmt.Printf("%s\n", card)
 		cardNumbers := strings.Split(card, " ")
-		// fmt.Printf("%s\n", cardNumbers[0])
 		cardScore := 0
 		for _, cardNumber := range cardNumbers {
-			// fmt.Printf("'%s'\n", cardNumber)
 			var intCard int
 			if cardNumber != "" {
 				// fmt.Printf("card number '%s'\n", cardNumber)
@@ -53,7 +46,6 @@ func main() {
 			for _, winnerNumber := range winnerNumbers {
 				var intWinner int
 				if winnerNumber != "" {
-					// fmt.Printf("Test '%s', against '%s'\n", cardNumber, winnerNumber)
 					intWinner, err = strconv.Atoi(winnerNumber)
 					check(err)
 				}
@@ -61,22 +53,34 @@ func main() {
 					if intWinner != 0 {
 						if intCard == intWinner {
 							fmt.Printf("'%d','%d' MATCH!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n", intCard, intWinner)
-							if cardScore == 0 {
-								cardScore = 1
-							} else {
-							cardScore = cardScore*2
-							}
+								cardScore = cardScore + 1
 						}
 					}
 				}
 			}
 		}
-		// fmt.Printf("%s \n", winnerNumbers)
-		// fmt.Printf("%s \n", cardNumbers)
+		fmt.Printf("totalCount is: %d \n", totalCount)
+		var totalCountAmmend []int
+		if len(totalCount) < cardCount + cardScore {
+			var appendLoop int 
+			var arrayAppend []int
+			appendLoop = cardCount + cardScore - len(totalCount)
+			for i := 1; i < appendLoop; i++ {
+				arrayAppend[0] = 0
+				totalCountAmmend = append(totalCountAmmend, arrayAppend...)
+			}
+			totalCount = append(totalCount, totalCountAmmend...)
+		}
+		fmt.Printf("totalCount is: %d \n", totalCount)
 		fmt.Printf("Card %d total score is %d\n", cardCount, cardScore)
+		for currentCount := 1; currentCount < cardScore + 1; currentCount++ {
+			totalCount[cardCount + currentCount] = totalCount[cardCount + currentCount] + 1
+		}
 		cardCount = cardCount + 1
-		totalScore = totalScore + cardScore
-		cardScore = 0
+		// cardScore = 0
+	}
+	for _, counts := range totalCount {
+		totalScore = totalScore + counts
 	}
 	fmt.Printf("Total score: %d\n", totalScore)
 }
