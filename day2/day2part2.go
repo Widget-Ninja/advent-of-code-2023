386325bca51e25cbb046c1d8632fc7482dd6923f
Widget-Ninja <shamus.teh.jackal@gmail.com>
2023-12-08 16:38:21 -0600

    Finished day2part2, template day3 and day5

--- day2/day2part2.go
+++ day2/day2part2.go
@@ -26,13 +26,15 @@ func main() {
     for fileScan.Scan() {
         inputLines = append(inputLines, fileScan.Text())
     }
-	var totalGameNumbers int
+	// var totalGameNumbers int
+	var gameWorkNumber int
 	// var enoughDice bool
 	for _, line := range inputLines {
 		var lineSplit []string
 		var gameNumber int
 		var gameCount int
-		enoughDiceArray := []int{0,0,0,0,0,0,0}
+		var powerDice int
+		// minDiceArray := []int{0, 0, 0, 0}
 		lineSplit = strings.Split(line, ": ")
 		// fmt.Printf("%s\n %s\n", lineSplit[0], lineSplit[1])
 		for _, findGame := range lineSplit {
@@ -52,12 +54,13 @@ func main() {
 			game = strings.Split(games, "; ")
 			// fmt.Printf("Game Number: %d\n", gameNumber)
 			gameCount = 0
+			greenTotal, redTotal, blueTotal := 0, 0, 0
 			for _, colors := range game {
 				var color []string
 				var greenBool, redBool, blueBool bool
 				var greenStr, redStr, blueStr string
 				var greenDice, redDice, blueDice int
-				var greenTotal, redTotal, blueTotal int
+				
 				color = strings.Split(colors, ", ")
 				for _, numberColors := range color {
 					// fmt.Printf("%s\n", numberColors)
@@ -67,63 +70,68 @@ func main() {
 					if greenBool == true {
 						greenStr = findNumber.FindString(numberColors)
 						greenDice, err = strconv.Atoi(string(greenStr))
-						greenTotal = greenTotal + greenDice
+						// greenTotal = greenTotal + greenDice
 						// fmt.Printf("Green Total: %d\n", greenTotal)
 					}
 					if redBool == true {
 						redStr = findNumber.FindString(numberColors)
 						redDice, err = strconv.Atoi(string(redStr))
-						redTotal = redTotal + redDice
+						// redTotal = redTotal + redDice
 						// fmt.Printf("Red Total: %d\n", redTotal)
 					}
 					if blueBool == true {
 						blueStr = findNumber.FindString(numberColors)
 						blueDice, err = strconv.Atoi(string(blueStr))
-						blueTotal = blueTotal + blueDice
+						// blueTotal = blueTotal + blueDice
 						// fmt.Printf("Blue Total: %d\n", blueTotal)
 					}
 				}
-				if greenTotal > 13 {
+				if greenDice > greenTotal {
 					// enoughDice = false
-					enoughDiceArray[gameCount] = 0
-					fmt.Printf("Green Failed!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n")
-				} else if blueTotal > 14 {
-					// enoughDice = false
-					fmt.Printf("Blue Failed!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n")
-					enoughDiceArray[gameCount] = 0
-				} else if redTotal > 12 {
-					// enoughDice = false
-					fmt.Printf("Red Failed!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n")
-					enoughDiceArray[gameCount] = 0
-				} else {
-					// enoughDice = true
-					enoughDiceArray[gameCount] = 1
+					greenTotal = greenDice
+					fmt.Printf("Green Updated to %d\n", greenTotal)
 				}
-				fmt.Printf("enoughDiceArray:\n")
-				fmt.Printf("%v\n", enoughDiceArray)
+				if blueDice > blueTotal {
+					// enoughDice = false
+					blueTotal = blueDice
+					fmt.Printf("Blue Updated to %d\n", blueTotal)
+				}
+				if redDice > redTotal {
+					// enoughDice = false
+					redTotal = redDice
+					fmt.Printf("Red Updated to %d\n", greenTotal)
+				} 
+				// fmt.Printf("minDiceArray:\n")
 				gameCount = gameCount + 1
 			}
+			fmt.Printf("green: %d, blue: %d, red: %d\n", greenTotal, blueTotal, redTotal)
+			powerDice = greenTotal * blueTotal * redTotal
+			fmt.Printf("powerDice: %d\n", powerDice)
+			// arrayAppend := []int{0}
+			// minDiceArray = append(minDiceArray, arrayAppend...)
+			// minDiceArray[gameNumber] = powerDice
+			// fmt.Printf("minDiceArray: %v\n", minDiceArray)
 		}
-		var gameWorkNumber int
-		gameWork := true
-		for _, dice := range enoughDiceArray {
+		
+		// gameWork := true
+		// for _, dice := range minDiceArray {
 			// fmt.Printf("dice: %d\n", dice)
-			gameWorkNumber = gameWorkNumber + dice
+		gameWorkNumber = gameWorkNumber + powerDice
 			// fmt.Printf("Before division gameWorkNumber: %d\n", gameWorkNumber)
-		}
+		// }
 		fmt.Printf("gameCount: %d\n", gameCount)
-		gameWorkNumber = gameWorkNumber / gameCount
-		// fmt.Printf("After division gameWorkNumber: %d\n", gameWorkNumber)
-		if gameWorkNumber == 1 {
-			gameWork = true
-		} else {
-			gameWork = false
-		}
+		// gameWorkNumber = gameWorkNumber / gameCount
+		fmt.Printf("gameWorkNumber: %d\n", gameWorkNumber)
+		// if gameWorkNumber == 1 {
+		// 	gameWork = true
+		// } else {
+		// 	gameWork = false
+		// }
 		// fmt.Printf("gameWork: %t\n", gameWork)
-		if gameWork == true {
-			fmt.Printf("Game %d is possible!\n", gameNumber)
-			totalGameNumbers = totalGameNumbers + gameNumber
-		}
+		// if gameWork == true {
+		// 	fmt.Printf("Game %d is possible!\n", gameNumber)
+		// 	totalGameNumbers = totalGameNumbers + gameNumber
+		// }
 	}
-	fmt.Print(totalGameNumbers)
+	fmt.Print(gameWorkNumber)
 }
---
+++ day5/day5.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"bufio"
+)
+
+func check(e error) {
+    if e != nil {
+        panic(e)
+    }
+}
+
+func main() {
+	input, err := os.Open("C:/Users/shamu/Documents/Code/advent-of-code-2023/day3/input.txt")
+	check(err)
+
+	fileScan := bufio.NewScanner(input)
+    fileScan.Split(bufio.ScanLines)
+	var inputLines []string
+    for fileScan.Scan() {
+        inputLines = append(inputLines, fileScan.Text())
+    }
+	fmt.Print(inputLines)
+}
